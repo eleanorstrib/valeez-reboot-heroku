5fb6cbe125b0ec55c688c337c171f3aa18d1c56a
eleanorstrib <eleanor@sloan.mit.edu>
2015-10-20 16:00:30 -0700

    updates to data model, getting closer

--- valeezapp/models.py
+++ valeezapp/models.py
@@ -1,20 +1,39 @@
 from django.db import models as m
 import datetime
 
-class User(models.Model):
+class Users(m.Model):
+	userid = m.AutoField(primary_key=true)
 	username = m.CharField(max_length=25)
 	password = m.CharField(max_length=12)
 	email = m.CharField(max_length=75)
-	mobile = m.IntegerField(maxlength=11)
-	home_timezone = m.CharField(max_length=30)
+	mobile = m.PositiveIntegerField(maxlength=11)
+	home_timezone = m.CharField(max_length=40)
 
-class Garment(models.Model):
+class Trip(m.model):
+	destination = m.CharField(max_length=40)
+	depart_date = m.TimeField
+	return_date = m.TimeField
+	TRIP_TYPE_CHOICES = (
+		(type_bformal, 'Business formal'),
+		(type_bcasual, 'Business casual'),
+		(type_vacation, 'Vacation'),
+		)
+	trip_type = m.CharField(choices=TRIP_TYPE_CHOICES)
+
+class Garments(m.Model):
 	name = m.CharField(max_length=75)
-	male_wardrobe = m.BooleanField
-	female_wardrobe = m.BooleanField
-	layer = m.IntegerField
+	male = m.BooleanField
+	female = m.BooleanField
+	layer = m.PositiveIntegerField(lower=0, upper=5)
 	type_bformal = m.BooleanField
 	type_bcasual = m.BooleanField
 	type_vacation = m. BooleanField
 	rain = m.BooleanField
 	snow = m.BooleanField
+	icon = m.ImageField (height_field=100, widthfield=100)
+
+class Toiletries(m.Model):
+	male = m.BooleanField
+	female = m.BooleanField
+	trip_duration = m.PositiveIntegerField
+	icon = m.ImageField
