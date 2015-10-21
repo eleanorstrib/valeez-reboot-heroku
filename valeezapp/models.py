953b937ce810a9ba5e839cc2ef9e85006f6f0fce
eleanorstrib <eleanor@sloan.mit.edu>
2015-10-20 17:48:17 -0700

    added __str__ methods to all classes, primary keys, many to many relationship btw garments, toilets and valeez

--- valeezapp/models.py
+++ valeezapp/models.py
@@ -2,14 +2,26 @@ from django.db import models as m
 import datetime
 
 class Users(m.Model):
-	userid = m.AutoField(primary_key=true)
+	user_id = m.AutoField(primary_key=true)
 	username = m.CharField(max_length=25)
 	password = m.CharField(max_length=12)
+	GENDER_CHOICES = (
+		(male, "male"),
+		(female, "female"),
+		)
+	gender = m.CharField(choices=GENDER_CHOICES)
 	email = m.CharField(max_length=75)
 	mobile = m.PositiveIntegerField(maxlength=11)
 	home_timezone = m.CharField(max_length=40)
 
-class Trip(m.model):
+	def __str__(self):
+		return 'user_id %s username %s password %s gender %s email %s mobile %s timezone %s' 
+		% (self.user_id, self.username, self.password, self.gender, self.email, self.mobile, 
+			self.home_timezone)
+
+class Trip(m.Model):
+	trip_id = m.AutoField(primary_key=true)
+	user_id = m.ForeignKey('User.user_id')
 	destination = m.CharField(max_length=40)
 	depart_date = m.TimeField
 	return_date = m.TimeField
@@ -19,8 +31,27 @@ class Trip(m.model):
 		(type_vacation, 'Vacation'),
 		)
 	trip_type = m.CharField(choices=TRIP_TYPE_CHOICES)
+	
+	def __str__(self):
+		return 'trip_id %s user_id %s destination %s depart_date %s return_date %s trip_type %s'
+		 % (self.trip_id, self.user_id, self.destination, self.depart_date, self.return_date, 
+		 	self.trip_type)
+
+class Valeez(m.Model):
+	valeez_id = m.AutoField(primary_key=true)
+	user_id = m.ForeignKey('User.user_id')
+	trip_id = m.ForeignKey('Trip.trip_id')
+	garments = m.ManyToManyField(Garments)
+	toiletries = m.ManyToManyField(Toiletries)
+
+	def __str__(self):
+		return 'primary %s user_id %s trip_id %s garments %s toiletries %s' % 
+			(self.valeez_id, self.user_id, 
+			self.trip_id, self.garments, self.toiletries)
+
 
 class Garments(m.Model):
+	garment_id = m.AutoField(primary_key=true)
 	name = m.CharField(max_length=75)
 	male = m.BooleanField
 	female = m.BooleanField
@@ -30,10 +61,20 @@ class Garments(m.Model):
 	type_vacation = m. BooleanField
 	rain = m.BooleanField
 	snow = m.BooleanField
-	icon = m.ImageField (height_field=100, widthfield=100)
+	icon = m.ImageField (height_field=100, width_field=100)
+
+	def __str__(self):
+		return 'name %s female %s layer %s type_bformal %s type_bcasual %s type_vacation' 
+		% (self.name, self.female, self.layer, self.type_bformal, 
+			self.type_bcasual, self.type_vacation)
 
 class Toiletries(m.Model):
+	toiletries_id = m.AutoField(primary_key=true)
+	name = m.CharField(max_length=50)
 	male = m.BooleanField
 	female = m.BooleanField
 	trip_duration = m.PositiveIntegerField
 	icon = m.ImageField
+
+	def __str__(self):
+		return 'name %s female %s trip_duration %s'
