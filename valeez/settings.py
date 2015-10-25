83b717347d87a843e6ebd0e01e971e1a815a3f31
eleanorstrib <eleanor@sloan.mit.edu>
2015-10-24 17:27:17 -0700

    added env variables and confirmed they work, but manage.py can't read them

--- valeez/settings.py
+++ valeez/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import sys
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-4mwg)j1xa9$@6*1$wnmmgf*#(zv&4lz*d-y%0@znaz&!=zbxz'
+SECRET_KEY = os.environ.get(DJANGO_SECRET_KEY, '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,10 +81,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'valeezdb',
-        'USER': 'eleanor',
-        'PASSWORD': '14Arlo14',
+        'USER':  os.environ.get(DB_USERNAME, ''),
+        'PASSWORD': os.environ.get(DB_PASSWORD, ''),
         'HOST': 'localhost',
-        'PORT': '5432',
+        'PORT': '',
     }
 }
 
