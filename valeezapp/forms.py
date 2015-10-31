ff4d783deb3f6a32409ce92c7688fbbab998e95e
eleanorstrib <eleanor@sloan.mit.edu>
2015-10-30 21:23:18 -0700

    added bare bones of user sign up form and new view to show it

--- valeezapp/forms.py
+++ valeezapp/forms.py
@@ -5,3 +5,9 @@ class VoyageForm(forms.ModelForm):
 	class Meta:
 		model = Voyage
 		fields = ('destination', 'depart_date', 'return_date', 'voyage_type',)
+
+
+class SignUpUserForm(forms.ModelForm):
+	class Meta:
+		model = User
+		fields = ('username', 'password', 'gender', 'email', 'mobile', 'home_timezone')
--- valeezapp/views.py
+++ valeezapp/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
-from valeezapp.models import Voyage
-from .forms import VoyageForm
+from valeezapp.models import Voyage, User
+from .forms import VoyageForm, SignUpUserForm
+
 
 def index(request):
 	return render(request, 'valeezapp/index.html', {})
+
 
 def make_valeez(request):
 	form = VoyageForm()
@@ -13,8 +15,17 @@ def make_valeez(request):
 		form = VoyageForm(request.POST)
 		if form.is_valid():
 			form.save()
-	
 	return render(request, 'valeezapp/make_valeez.html', {'form': form})
+
+
+def sign_up(request):
+	form = SignUpUserForm()
+	if request.method == 'POST':
+		form = SignUpUserForm(request.POST)
+		if form.is_valid():
+			form.save()
+	return render(request, 'valeezapp/sign_up.html', {'form': form})
+
 
 def past_voyages(request):
 	voyages = Voyage.objects.order_by('destination')
