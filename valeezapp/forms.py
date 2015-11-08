996653028c1a9c44556fe2daa3312abb0318f5dd
eleanorstrib <eleanor@sloan.mit.edu>
2015-11-07 16:53:47 -0800

    added validation on Voyage form to ensure that departure date not in past and return date > than departure date

--- valeezapp/forms.py
+++ valeezapp/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import UserProfile, Voyage
 from functools import partial 
+import datetime as dt
 DateInput = partial(forms.DateInput, {'class': 'datepicker'})
 
 class UserForm(forms.ModelForm):
@@ -19,6 +20,19 @@ class VoyageForm(forms.ModelForm):
 		widgets = {'depart_date': DateInput(), 'return_date': DateInput()}
 		model = Voyage
 		fields = ('destination', 'depart_date', 'return_date', 'voyage_type', 'gender')
+
+	def clean(self):
+		cleaned_dates = super(VoyageForm, self).clean()
+		depart_date = cleaned_dates.get('depart_date')
+		return_date = cleaned_dates.get('return_date')
+		today = dt.date.today()
+
+		if depart_date > return_date:
+			raise forms.ValidationError('Oops - try again! Your return date must be after your departure date.')
+
+		if depart_date < today:
+			raise forms.ValidationError('Oops - try again! Your departure date must be in the future.')
 		
+
 
 
