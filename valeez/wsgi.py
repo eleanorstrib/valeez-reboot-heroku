19315f3619b300c2e042d77dd17fe287bef86167
eleanorstrib <eleanor@sloan.mit.edu>
2016-01-10 20:52:14 -0800

    removed local from wsgi, manage.py in valeez -- crashed app

--- valeez/wsgi.py
+++ valeez/wsgi.py
@@ -3,6 +3,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "valeez.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "valeez.settings")
 
 application = get_wsgi_application()
