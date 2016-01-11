b0fa664b933671a353564eee355ca57446e5c729
eleanorstrib <eleanor@sloan.mit.edu>
2016-01-10 22:17:59 -0800

    changed allowed hosts local setting to 127.0.0

--- valeez/settings.py
+++ valeez/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', 'evening-earth-7242.herokuapp.com', 'valeez.com']
+ALLOWED_HOSTS = ['127.0.0', 'evening-earth-7242.herokuapp.com', 'valeez.com']
 
 SITE_ID = 2
 # Application definition
