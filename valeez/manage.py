2203e918e3d483d652b9f95177bbbe1ede034a61
eleanorstrib <eleanor@sloan.mit.edu>
2016-01-10 21:14:59 -0800

    trying to run removing local

--- valeez/manage.py
+++ valeez/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "valeez.settings.local")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "valeez.settings")
 
     from django.core.management import execute_from_command_line
 
