44c6e967ca69e8b2013a382f2b12b2b12c9649cb
eleanorstrib <eleanor@sloan.mit.edu>
2016-01-10 21:20:49 -0800

    updated settings ALLOWED_HOSTS to include heroku app name/url

--- valeez/settings.py
+++ valeez/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'evening-earth-7242.herokuapp.com', 'valeez.com']
 
 SITE_ID = 2
 # Application definition
