ef7419acf1b95cf739ae853e65b70c865597cb50
eleanorstrib <eleanor@sloan.mit.edu>
2016-01-10 21:35:17 -0800

    added whitenoise config

--- valeez/settings.py
+++ valeez/settings.py
@@ -89,8 +89,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+# -- these are the original static url and root
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# these are for heroku deployment
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 
 # registration redux
 REGISTRATION_OPEN = True
--- valeez/wsgi.py
+++ valeez/wsgi.py
@@ -1,8 +1,9 @@
 
 import os
-
+from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "valeez.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
