4ddf25a2737d8943a2a8a7dae483e0dd36ca9d9a
eleanorstrib <eleanor@sloan.mit.edu>
2016-01-10 21:47:34 -0800

    fixed import order in wsgi file

--- valeez/wsgi.py
+++ valeez/wsgi.py
@@ -1,7 +1,7 @@
 
 import os
-from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "valeez.settings")
 
